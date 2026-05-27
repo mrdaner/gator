9a9a8f3866f45b15effbcf1e3a7505164da877ff
mrdaner <lakhnodima@gmail.com>
2026-05-27 20:39:21 +0000

    commands.ts

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,1 +1,16 @@
-type CommandHandler = (cmdName: string, ...args: string[]) => void;
+export type CommandHandler = (cmdName: string, ...args: string[]) => void;
+
+export type CommandsRegistry = Record<string, CommandHandler>;
+
+export function registerCommand(registry: CommandsRegistry, cmdName: string, Handler: CommandHandler) {
+	registry[cmdName] = Handler;
+};
+
+export function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]) {
+	const handler = registry[cmdName]
+	if (!handler) {
+		throw new Error("Command isn't registered")
+	}
+	handler(cmdName, ...args)
+}
+
